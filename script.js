d3c1197e8b5029ddb6cfeb0a0c039514ff83b178
Abhinav Kumar <abhi.kumar778@gmail.com>
2025-02-13 23:36:24 +0100

    valentine done

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
       "6months": new Date("February 7, 2025 08:00:00"),
       // "birthday": new Date("January 27, 2025 00:00:00"),
       "birthday": new Date("February 10, 2025 20:00:00"),
-      "valentine": new Date("February 14, 2025 20:00:00")
+      "valentine": new Date("February 10, 2025 20:00:00")
     };
 
     Object.keys(eventDates).forEach(eventId => {
@@ -118,3 +118,9 @@ prevArrow.addEventListener("click", prevSlide);
 showSlide(currentSlideIndex);
 
 
+// Show Sad Image on "No"
+function showSadImage() {
+  console.log("showSad")
+  document.getElementById("valentine-screen").classList.add("hidden");
+  document.getElementById("sad-image").style.display = "flex";
+}
